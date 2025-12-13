b7cc929b8d2b87c1e8dd79ed84af0798ac83ea25
SAMKIEL <samuelezekiel488@gmail.com>
2025-12-13 22:30:51 +0100

    chore: update bot's WhatsApp number in settings

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ const settings = {
   botName: "𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋",
   botOwner: "ѕαмкιєℓ.∂єν", // Your name
   ownerNumber: jidNormalizedUser("2348087357158@s.whatsapp.net"), // Normalized JID for owner
-  botNumber: "2348087357158", // Set the bot's WhatsApp number for pairing (without + symbol, just country code & number)
+  botNumber: "2348050694825", // Set the bot's WhatsApp number for pairing (without + symbol, just country code & number)
   giphyApiKey: "qnl7ssQChTdPjsKta2Ax2LMaGXz303tq",
   commandMode: "public",
   description:
