c10868885e405af943e2ac76f2a9b39c36da26e2
Samuel Ezekiel <142183985+samkiell@users.noreply.github.com>
2025-12-14 10:19:02 +0100

    Set botNumber to 2348050694825

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   author: "‎",
   botName: "𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋",
   botOwner: "ѕαмкιєℓ.∂єν", // Your name
-  ownerNumber: jidNormalizedUser("2348087357158@s.whatsapp.net"), // Normalized JID for owner
+  ownerNumber: jidNormalizedUser("2348050694825@s.whatsapp.net"), // Normalized JID for owner
   botNumber: "2348050694825", // Set the bot's WhatsApp number for pairing (without + symbol, just country code & number)
   giphyApiKey: "qnl7ssQChTdPjsKta2Ax2LMaGXz303tq",
   commandMode: "public",
