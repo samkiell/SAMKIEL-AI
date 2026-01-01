c08100d3f8a86bf8f4175548628ce64f42205673
SAMKIEL <samuelezekiel488@gmail.com>
2026-01-01 14:28:37 +0100

    feat: introduce pair command for generating WhatsApp pairing codes

--- commands/pair.js
+++ commands/pair.js
@@ -39,7 +39,6 @@ async function pairCommand(sock, chatId, message, q) {
 
       await sock.sendMessage(chatId, {
         text: "Wait a moment for the code",
-        ...global.channelInfo,
       });
 
       try {
@@ -56,7 +55,6 @@ async function pairCommand(sock, chatId, message, q) {
           await sleep(5000);
           await sock.sendMessage(chatId, {
             text: `Your pairing code: ${code}`,
-            ...global.channelInfo,
           });
         } else {
           throw new Error("Invalid response from server");
