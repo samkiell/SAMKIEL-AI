6675abd288b03cef8e8f2d31288cefe3d06927e2
SAMKIEL <samuelezekiel488@gmail.com>
2026-01-03 08:38:54 +0100

    refactor: Centralize bot name, update bot and owner contact information, reorder feature toggles, and modify update URL.

--- settings.js
+++ settings.js
@@ -1,34 +1,33 @@
+const botName = "SAMKIEL BOT";
+
 const settings = {
-  // Core Foundation
-  botName: "My Awesome Bot",
+  // Core Foundationn
+  botName: botName,
   prefix: ".",
-  botNumber: "2348012345678",
-  ownerNumber: "2348012345678",
-  ownerName: "Admin 𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋",
+  botNumber: " ",
+  ownerNumber: "2348087357158",
+  ownerName: "SAMKIEL",
   developer: "ѕαмкιєℓ.∂єν",
   portfolio: "https://samkiel.dev",
   website: "https://samkielbot.app",
 
-  // Feature Toggles (Configured by Platform)
+  // Feature Toggles
   featureToggles: {
-    // Privacy & Security
-    COMMAND_MODE: "public", // "public" or "private"
-    REJECT_CALL: false, // true/false
-    ANTI_DELETE: false, // true/false
-    ANTI_DELETE_TYPE: "all", // "all" or "dm"
-
-    // Activity & Automation
-    ALWAYS_ONLINE: true, // true/false
-    SEND_READ: false, // true/false (Blue Ticks)
-    AUTO_STATUS_VIEW: "on", // "on" or "off"
-    STATUS_VIEW_EMOJI: "👀", // Emoji for status
-    STATUS_VIEW_MSG: "off", // "off" or text
-    PERSONAL_MESSAGE: false, // true/false (Enable in DM)
-    DISABLE_START_MESSAGE: false, // true/false (Start message)
-    RANKING: false, // true/false
-    AUTO_REACTION: false, // true/false
+    AUTO_STATUS_VIEW: "off",
+    ENABLE_STATUS_REACTION: false,
+    STATUS_VIEW_EMOJI: "👀",
+    STATUS_VIEW_MSG: "off",
+    ANTI_DELETE: false,
+    SEND_READ: false,
+    ALWAYS_ONLINE: true,
+    REJECT_CALL: false,
+    PERSONAL_MESSAGE: false,
+    DISABLE_START_MESSAGE: false,
+    ANTI_DELETE_TYPE: "all",
+    COMMAND_MODE: "public",
+    RANKING: false,
+    AUTO_REACTION: false,
     PACKNAME: "𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋",
-    ENABLE_STATUS_REACTION: false, // Internal flag
   },
 
   // Metadata & Other Configs
@@ -37,8 +36,7 @@ const settings = {
   description:
     "This is a bot for managing group commands and automating tasks.",
   version: "2.7.0",
-  updateZipUrl:
-    "https://github.com/samkiell/SAMKIEL-AI/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/samkiell//archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
