828d254720ad4b9cd98651e3a64872d39fbc01c5
Samuel Ezekiel <142183985+samkiell@users.noreply.github.com>
2026-01-03 05:59:52 +0100

    Update settings for Bot 1111111111111

--- settings.js
+++ settings.js
@@ -1,34 +1,37 @@
-const botName = "𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋";
+const botName = "sonia";
 
 const settings = {
   // Core Foundationn
   botName: botName,
   prefix: ".",
-  botNumber: "",
+  botNumber: "1111111111111",
   ownerNumber: "2348087357158",
-  ownerName: "SAMKIEL",
+  ownerName: "Admin 𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋",
   developer: "ѕαмкιєℓ.∂єν",
   portfolio: "https://samkiel.dev",
   website: "https://samkielbot.app",
 
   // Feature Toggles
   featureToggles: {
-    AUTO_STATUS_VIEW: "on", // Options: "off", "on" (view only), "no-dl" (view only) - Auto-download disabled to save space
-    ENABLE_STATUS_REACTION: true, // Toggle for status reactions
-    STATUS_VIEW_EMOJI: "👀",
-    STATUS_VIEW_MSG: "off",
-    ANTI_DELETE: true,
-    SEND_READ: false,
-    ALWAYS_ONLINE: true,
-    REJECT_CALL: true,
-    PERSONAL_MESSAGE: false,
-    DISABLE_START_MESSAGE: false,
-    ANTI_DELETE_TYPE: "all", // Options: "all", "group", or "private"
-    COMMAND_MODE: "public",
-    RANKING: false,
-    AUTO_REACTION: false,
-    PACKNAME: "𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋",
-  },
+  AUTO_STATUS_VIEW: "off",
+  autoStatusView: "off",
+  ENABLE_STATUS_REACTION: false,
+  STATUS_VIEW_EMOJI: "👀",
+  STATUS_VIEW_MSG: "off",
+  ANTI_DELETE: false,
+  antiDelete: false,
+  SEND_READ: false,
+  ALWAYS_ONLINE: false,
+  REJECT_CALL: false,
+  PERSONAL_MESSAGE: false,
+  DISABLE_START_MESSAGE: false,
+  ANTI_DELETE_TYPE: "all",
+  antiDeleteType: "all",
+  COMMAND_MODE: "public",
+  RANKING: false,
+  AUTO_REACTION: false,
+  PACKNAME: "𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋"
+},
 
   // Metadata & Other Configs
   author: "𝕊𝔸𝕄𝕂𝕀𝔼𝕃 𝔹𝕆𝕋",
